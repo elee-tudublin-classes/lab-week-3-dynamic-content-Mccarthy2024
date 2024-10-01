0cc38b093f8d2e610b50eca2123cf29118c1aecb
X00202566 Sean Mc Carthy <X00202566@myTUDublin.ie>
2024-10-01 13:43:41 +0100

    Lab 2

---
+++ app/main.py
@@ -0,0 +1,35 @@
+# import dependencies
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+# create app instance
+app = FastAPI()
+
+# set location for templates
+templates = Jinja2Templates(directory="app/view_templates")
+
+# handle http get requests for the site root /
+# return the index.html page
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/advice.html", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="advice.html")
+
+@app.get("/params.html", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="params.html")
+
+@app.get("/dependencies.html", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="dependencies.html") 
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static",
+)
